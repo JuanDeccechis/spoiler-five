6d7cc93c810684ac73fb8444f03b0290f963ebd9
Juan Cruz Deccechis <juandeccechis@gmail.com>
2021-02-17 00:19:51 -0300

    config for launch react app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import store from './store.js'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
@@ -14,4 +13,3 @@ ReactDOM.render(
     </Provider>
   </Router>
 , document.getElementById('root'));
-serviceWorker.unregister();
