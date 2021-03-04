2d5f8f837654e2c1d2a15362c9a2438035c31bb4
Juan Cruz Deccechis <juandeccechis@gmail.com>
2021-03-03 23:18:31 -0300

    formulario de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -78,59 +78,20 @@ class Login extends Component {
                                 <input id="password" ref="password" required/>
 
                             </div>
-                            <div className="login-input">
+                            <div>
                                 <Link to="/" ref="linkToHome" onClick={this.handleClickSignIn}>
-                                <button type="button">Iniciar</button>
+                                <button type="button">Iniciar sesion</button>
                                 </Link>
+                            </div>
+                            <div>
+                                <span className="register-invitation">Tambien podes registrarte</span>
                                 <Link to="/" >
                                 <button type="button" className="primary-button" onClick={this.handleClickSignIn}>Registrarse</button>
                                 </Link>
                             </div>
-                            <button>obtener plan</button>
 
                         </form>
-                        
-
-<button onClick={this.showIcons}>show icons</button>
-    {show &&
-    <div>                    <div className="player2" >
-                            <button className="icon star-complete"></button>
-                            <button className="icon like"></button>
-                            <button className="icon like-complete"></button>
-                            <button className="icon heart"></button>
-                            <button className="icon heart-complete"></button>
-                            <button className="icon dislike"></button>
-                            <button className="icon dislike-complete"></button>
-                            <button className="icon share"></button>
-                            <button className="icon add"></button>
-                            <button className="icon delete"></button>
-                            <button className="icon show-more"></button>
-                        </div>
-                        <div className="player2" >
-                            <button className="icon question"></button>
-                            <button className="icon setting"></button>
-                            <button className="icon menu-playlist"></button>
-                            <button className="icon menu-playlist2"></button>
-                            <button className="icon menu-album"></button>
-                            <button className="icon menu-podcast"></button>
-                            <button className="icon menu-song"></button>
-                            <button className="icon menu-favorites"></button>
-                        </div>
-                        <div className="player2" >
-                            <button className="icon menu-icon"></button>
-                            <button className="icon previous"></button>
-                            <button className="icon primary pause"></button>
-                            <button className="icon primary play"></button>
-                            <button className="icon next"></button>
-                            <button className="icon notification-yes"></button>
-                            <button className="icon notification"></button>
-                            <button className="icon refresh"></button>
-                            <button className="icon loading"></button>
-                            <button className="icon search"></button>
-                            <button className="icon search-black"></button>
-                            <button className="icon user"></button>
-                        </div>
-                        </div>        }              </div>
+                    </div>
                 </div>
             </div>
 
